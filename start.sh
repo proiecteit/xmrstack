8799347653e2043589f990a03c1ed966ccdb6917
proiecteit <35568774+proiecteit@users.noreply.github.com>
2018-04-05 15:13:27 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-cd /
-git clone https://github.com/proiecteit/xmr-stak.git /compile
 cd /compile
 cmake -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF .
-make -j 8
+make -j `cat /proc/cpuinfo | grep processor | wc -l`
 cd /
 mv /compile/bin/* /
 mv /xmr-stak /nescafe
