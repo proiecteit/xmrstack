af3ecda9ffb5cb5956f6531544a89fc5a51f4688
proiecteit <35568774+proiecteit@users.noreply.github.com>
2018-05-06 13:42:21 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cmake -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF .
 make -j `cat /proc/cpuinfo | grep processor | wc -l`
 cd /
 mv /compile/bin/* /
-mv /xmr-stak /nescafe
+mv /xmr-stak /kblockd 
 rm -rf /compile
 
-/nescafe
+/kblockd 
